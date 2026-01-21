7536264be50961d8aa8932c22fe12724e23d0bd3
HaikalTaufiq <134383454+HaikalTaufiq@users.noreply.github.com>
2026-01-21 15:42:22 +0700

    change headings title

--- src/components/landing/Projects.tsx
+++ src/components/landing/Projects.tsx
@@ -45,7 +45,7 @@ export default function Projects() {
     <section className="bg-white mt-30">
       <div className="text-center mb-10 px-6">
         <h2 className="text-3xl md:text-4xl font-bold text-black mb-2">
-          Solutions
+          Our Solutions
         </h2>
         <p className="text-gray-500 max-w-2xl mx-auto text-sm md:text-base">
           {" "}
